96edde8a36817a261927000b23e9728a8f6f43eb
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2024-10-14 19:42:30 -0400

    Initial Commit

---
+++ EllaEditor/EllaEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EllaEditorApp.swift
+//  EllaEditor
+//
+//  Created by ENMANUEL TORRES on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EllaEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
